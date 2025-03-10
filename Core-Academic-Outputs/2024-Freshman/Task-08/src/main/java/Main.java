2c306c2137f3040eaa2aa06abbd183f5652f5692
Ransel Sumatra <dranrebsumatra@gmail.com>
2025-03-11 01:18:37 +0800

    Task-08 folder

---
+++ Core-Academic-Outputs/2024-Freshman/Task-08/src/main/java/Main.java
@@ -0,0 +1,21 @@
+/**
+ * This code is made by Ransel Sumatra.
+ * GitHub: https://github.com/ranselcrackers/
+ * GitHub Username: ranselcrackers
+ * Specialization: BSIT Major in Marketing Technologies at Mapúa Malayan Colleges Laguna
+ */
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a list to store employees
+        ArrayList<Employee> employees = EmployeeDataInitializer.initializeEmployees();
+
+        // Display employee information
+        for (Employee employee : employees) {
+            employee.displayEmployeeInfo();
+            System.out.println("\n"); // Add space between tables
+        }
+    }
+}
