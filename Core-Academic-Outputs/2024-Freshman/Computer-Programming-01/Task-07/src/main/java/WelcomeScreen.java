6ac76f0c2a885e12d65165b89246ece3703a4ea3
Drew Guinto <drewguinto09@gmail.com>
2025-03-11 21:30:36 -0700

    Update WelcomeScreen.java
    March 12 2025 Drew made changes

--- Core-Academic-Outputs/2024-Freshman/Computer-Programming-01/Task-07/src/main/java/WelcomeScreen.java
+++ Core-Academic-Outputs/2024-Freshman/Computer-Programming-01/Task-07/src/main/java/WelcomeScreen.java
@@ -1,20 +1,25 @@
 public class WelcomeScreen {
     public static void displayWelcomeScreen() {
-        System.out.println("+-------------------------------------------------------------+");
-        System.out.println("|                                                             |");
-        System.out.println("|                  WELCOME TO MOTORPH                         |");
-        System.out.println("|                  Employee Information Dashboard             |");
-        System.out.println("|                                                             |");
-        System.out.println("+-------------------------------------------------------------+");
-        System.out.println("|                                                             |");
-        System.out.println("|  Hello! This is the MotorPH Employee Information Dashboard. |");
-        System.out.println("|  Here, you can view detailed information about employees.   |");
-        System.out.println("|                                                             |");
-        System.out.println("+-------------------------------------------------------------+");
-        System.out.println("|                                                             |");
-        System.out.println("|  Let's get started!                                         |");
-        System.out.println("|                                                             |");
-        System.out.println("+-------------------------------------------------------------+");
+        System.out.println("+----------------------------------------------------------------------------------+");
+        System.out.println("|        __  __       _             ____  _   _                                    |");
+        System.out.println("|       |  \/  | ___ | |_ ___  _ __|  _ \| | | |                                   |");
+        System.out.println("|       | |\/| |/ _ \| __/ _ \| '__| |_) | |_| |                                   |");
+        System.out.println("|       | |  | | (_) | || (_) | |  |  __/|  _  |                                   |");
+        System.out.println("|       |_|  |_|\___/ \__\___/|_|  |_|   |_| |_|                                   |");
+        System.out.println("|                                                                                  |");
+        System.out.println("|       7 Jupiter Avenue cor. F. Sandoval Jr., Bagong Nayon, Quezon City           |");
+        System.out.println("|       Phone: (028) 911-5071 / (028) 911-5072 / (028) 911-5073                    |");
+        System.out.println("|       Email: corporate@motorph.com                                               |");
+        System.out.println("+----------------------------------------------------------------------------------+");
+        System.out.println("|                                                                                  |");
+        System.out.println("|       Welcome to MotorPH Employee Dashboard                                      |");
+        System.out.println("|       Your Central Hub for Employee Information                                  |");
+        System.out.println("|                                                                                  |");
+        System.out.println("+----------------------------------------------------------------------------------+");
+        System.out.println("|                                                                                  |");
+        System.out.println("|       View your salary, deductions, and benefits with ease.                      |");
+        System.out.println("|                                                                                  |");
+        System.out.println("+----------------------------------------------------------------------------------+");
         System.out.println("\n");
     }
 }
